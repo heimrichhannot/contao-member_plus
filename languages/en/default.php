db456f4c62c0c1952a8db9ed53a0c5687ab839b4
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2016-09-07 12:16:05 +0200

    rename autologin from "Remember me" to "Stay signed in"

---
+++ languages/en/default.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2016 Heimrich & Hannot GmbH
+ *
+ * @author  Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
+ * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+ */
+
+/**
+ * Login form
+ */
+$GLOBALS['TL_LANG']['MSC']['autologin'] = 'Stay signed in'; // "Remember me" is not clear enough. (e.g. (remember password forever on the machine? remember the email...)
